acf71de5c4aae690b278b35bb6cd0559ce2be1cb
Kirill <yakubov.kirill.work@gmail.com>
2020-07-29 11:51:58 +0300

    switch-case

--- FirstApp/src/by/epam/learn/LearnJava.java
+++ FirstApp/src/by/epam/learn/LearnJava.java
@@ -1,5 +1,7 @@
 package by.epam.learn;
 
+import java.awt.image.AreaAveragingScaleFilter;
+
 public class LearnJava {
     public static void main(String[] args) {
         final int DISCOUNT = 10;
@@ -13,6 +15,26 @@ public class LearnJava {
         }
         else {
             cartCost = cart + DISCOUNT;
+        }
+
+        final int ARGS_LEN = args.length;
+        switch (ARGS_LEN) {
+            case 0: {
+                System.out.println("Args length is 0");
+                break;
+            }
+            case 1: {
+                System.out.println("Args length is 1");
+                break;
+            }
+            case 2: {
+                System.out.println("Args length is 2");
+                break;
+            }
+            default: {
+                System.out.println("There is too much parameters");
+                break;
+            }
         }
     }
 }
