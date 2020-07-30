4fdc399499af60bbe91317303bf4bedcccbbe304
Kirill <yakubov.kirill.work@gmail.com>
2020-07-30 12:06:03 +0300

    Main task 2

---
+++ FirstApp/src/by/epam/learn/MainTask2.java
@@ -0,0 +1,9 @@
+package by.epam.learn;
+
+public class MainTask2 {
+    public static void main(String[] args) {
+        for (int i = args.length - 1; i >= 0 ; i--) {
+            System.out.print(args[i] + " ");
+        }
+    }
+}
