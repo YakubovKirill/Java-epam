c01ece27aa64b3a75c24afc809272a085dd17964
Kirill <yakubov.kirill.work@gmail.com>
2020-08-07 10:17:50 +0300

    set

--- Collections/src/by/epam/learn/Main.java
+++ Collections/src/by/epam/learn/Main.java
@@ -1,7 +1,16 @@
 package by.epam.learn;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-
+        Set<String> stringSet = new HashSet<String>() {
+            {
+                this.add("One");
+                this.add("Two");
+            }
+        };
+        System.out.println(stringSet);
     }
 }
