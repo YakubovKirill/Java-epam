8f1529fdcf58aa4521ee51b11253a313feb6c127
Kirill <yakubov.kirill.work@gmail.com>
2020-08-07 10:28:46 +0300

    List

--- Collections/src/by/epam/learn/Main.java
+++ Collections/src/by/epam/learn/Main.java
@@ -1,6 +1,8 @@
 package by.epam.learn;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Main {
@@ -12,5 +14,18 @@ public class Main {
             }
         };
         System.out.println(stringSet);
+
+        List<Integer> intList = new ArrayList<>() {
+            {
+                this.add(1);
+                this.add(2);
+                this.add(4);
+                this.add(5);
+                this.add(null);
+            }
+        };
+        System.out.println(intList);
+        intList.remove(intList.indexOf(4));
+        System.out.println(intList);
     }
 }
