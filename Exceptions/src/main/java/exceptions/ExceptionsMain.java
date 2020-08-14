339979c31510ddf9588ba652a4924c5af7407079
Kirill <yakubov.kirill.work@gmail.com>
2020-08-14 14:08:33 +0300

    multicatch

--- Exceptions/src/main/java/exceptions/ExceptionsMain.java
+++ Exceptions/src/main/java/exceptions/ExceptionsMain.java
@@ -2,6 +2,16 @@ package exceptions;
 
 public class ExceptionsMain {
     public static void main(String[] args) {
-        
+        String [] numbers = {"23", "0", "Y*"};
+        int result;
+        int value = 0;
+        try {
+            result = Integer.parseInt(numbers[2]);
+            value = 100 / result;
+        } catch (NumberFormatException | ArithmeticException e) {
+            result = -1;
+            System.err.println(e);
+        }
+        System.out.println(value);
     }
 }
